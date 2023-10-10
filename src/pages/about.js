eeb7b1ff74733ba30d630be6ba23d536610937f0
nguyenchanhlong <chanhlong171002@gmail.com>
2023-10-10 15:55:04 +0700

    Navbar Practices

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div
+      style={{
+        display: 'center',
+        color: 'black',
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        height: '100vh'
+      }}
+    >
+      <h1>Longlonglonglglnlnlgngl</h1>
+      <h2>adaaasdasdasdasasdasd</h2>
+    </div>
+  );
+};
+
+export default About;
